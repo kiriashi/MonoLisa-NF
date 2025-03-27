20fdba2efd7f011a4b9ff46a78f530f6ca794d2b
Entity 1011 <goffingslander@gmail.com>
2025-03-27 11:52:53 +0100

    RC2

--- flake.nix
+++ flake.nix
@@ -21,85 +21,85 @@
                 pname = "MonoLisa Font";
                 version = "0.0.0";
 
-                MonoLisa-Black = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
+                MonoLisaBlack = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
                 {
                     url ="https://github.com/Entity1011/MonoLisa/raw/refs/heads/staging/MonoLisa/MonoLisa-Black.ttf";
                     sha256 = "sha256-43EW/BjNyH7rjklyxhRF49wKPZqTGHTCGqTS4zYiY7s=";
                 };
 
-                MonoLisa-BlackItalic = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
+                MonoLisaBlackItalic = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
                 {
                     url ="https://github.com/Entity1011/MonoLisa/raw/refs/heads/staging/MonoLisa/MonoLisa-BlackItalic.ttf";
                     sha256 = "sha256-tMePIOVHmhQYUTERz74dQ/y/2ZvBAkJHr99XTRCGQys=";
                 };
 
-                MonoLisa-Bold = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
+                MonoLisaBold = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
                 {
                     url ="https://github.com/Entity1011/MonoLisa/raw/refs/heads/staging/MonoLisa/MonoLisa-Bold.ttf";
                     sha256 = "sha256-h388DMtYf3pbKnGgv/MrcQ4wDVXWHoLwmHbyR+9BFzA=";
                 };
 
-                MonoLisa-BoldItalic = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
+                MonoLisaBoldItalic = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
                 {
                     url ="https://github.com/Entity1011/MonoLisa/raw/refs/heads/staging/MonoLisa/MonoLisa-BoldItalic.ttf";
                     sha256 = "sha256-VwN1f3hPGkcH9hwp/XPk62vXdeMRl37trJ7AEb1JZQU=";
                 };
 
-                MonoLisa-ExtraLight = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
+                MonoLisaExtraLight = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
                 {
                     url ="https://github.com/Entity1011/MonoLisa/raw/refs/heads/staging/MonoLisa/MonoLisa-ExtraLight.ttf";
                     sha256 = "sha256-mDxz/46yD0i1IVlFFKBFkedf2KXM8u+VLohgoOwOhy8=";
                 };
 
-                MonoLisa-ExtraLightItalic = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
+                MonoLisaExtraLightItalic = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
                 {
                     url ="https://github.com/Entity1011/MonoLisa/raw/refs/heads/staging/MonoLisa/MonoLisa-ExtraLightItalic.ttf";
                     sha256 = "sha256-BLX6pfBcX1hryDsFS1Qyx8NWX/bO7xVWJuhOwrbMefE=";
                 };
 
-                MonoLisa-Light = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
+                MonoLisaLight = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
                 {
                     url ="https://github.com/Entity1011/MonoLisa/raw/refs/heads/staging/MonoLisa/MonoLisa-Light.ttf";
                     sha256 = "sha256-C/nTsCLm4f8KfyJlIuoE2bp3HwhlIaynCvbgMsZUeQ8=";
                 };
 
-                MonoLisa-LightItalic = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
+                MonoLisaLightItalic = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
                 {
                     url ="https://github.com/Entity1011/MonoLisa/raw/refs/heads/staging/MonoLisa/MonoLisa-LightItalic.ttf";
                     sha256 = "sha256-zL24wxIOXuP3AHBrFWty3Q9ARqSo78VYVonuqOQtovY=";
                 };
 
-                MonoLisa-Medium = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
+                MonoLisaMedium = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
                 {
                     url ="https://github.com/Entity1011/MonoLisa/raw/refs/heads/staging/MonoLisa/MonoLisa-Medium.ttf";
                     sha256 = "sha256-U/sYdX+vuZxYhnsM6aibuiLFK9Omi+pkzTpV3DTB8+k=";
                 };
 
-                MonoLisa-MediumItalic = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
+                MonoLisaMediumItalic = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
                 {
                     url ="https://github.com/Entity1011/MonoLisa/raw/refs/heads/staging/MonoLisa/MonoLisa-MediumItalic.ttf";
                     sha256 = "sha256-a0xb8mbyjMumRkMwViHS86CWUODpUP/X8xeGS35tuAQ=";
                 };
 
-                MonoLisa-Regular = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
+                MonoLisaRegular = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
                 {
                     url ="https://github.com/Entity1011/MonoLisa/raw/refs/heads/staging/MonoLisa/MonoLisa-Regular.ttf";
                     sha256 = "sha256-SC6uPYCC4lBT4++4CFE2Nspiag8gG/dlvVWG6f0ZEnQ=";
                 };
 
-                MonoLisa-RegularItalic = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
+                MonoLisaRegularItalic = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
                 {
                     url ="https://github.com/Entity1011/MonoLisa/raw/refs/heads/staging/MonoLisa/MonoLisa-RegularItalic.ttf";
                     sha256 = "sha256-H33VUB0GxlsXjBQUl4beY/m06okRzATypdLwy/uMj/4=";
                 };
 
-                MonoLisa-Thin = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
+                MonoLisaThin = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
                 {
                     url ="https://github.com/Entity1011/MonoLisa/raw/refs/heads/staging/MonoLisa/MonoLisa-Thin.ttf";
                     sha256 = "sha256-syGILKSkvJZWik6XvJ2OxcTYPFB/llB6yLSdmtbQAIU=";
                 };
 
-                MonoLisa-ThinItalic = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
+                MonoLisaThinItalic = inputs.nixpkgs.legacyPackages.x86_64-linux.fetchurl
                 {
                     url ="https://github.com/Entity1011/MonoLisa/raw/refs/heads/staging/MonoLisa/MonoLisa-ThinItalic.ttf";
                     sha256 = "sha256-WU6VbfYaS/UZV1CeNcWdVM0PX4YbWUM1i5yIG8YLuks=";
@@ -108,20 +108,20 @@
                 dontUnpack = true;
                 installPhase = ''
                     mkdir -p $out/share/fonts/truetype
-                    cp $MonoLisa-Black $out/share/fonts/truetype/
-                    cp $MonoLisa-BlackItalic $out/share/fonts/truetype/
-                    cp $MonoLisa-Bold $out/share/fonts/truetype/
-                    cp $MonoLisa-BoldItalic $out/share/fonts/truetype/
-                    cp $MonoLisa-ExtraLight $out/share/fonts/truetype/
-                    cp $MonoLisa-ExtraLightItalic $out/share/fonts/truetype/
-                    cp $MonoLisa-Light $out/share/fonts/truetype/
-                    cp $MonoLisa-LightItalic $out/share/fonts/truetype/
-                    cp $MonoLisa-Medium $out/share/fonts/truetype/
-                    cp $MonoLisa-MediumItalic $out/share/fonts/truetype/
-                    cp $MonoLisa-Regular $out/share/fonts/truetype/
-                    cp $MonoLisa-RegularItalic $out/share/fonts/truetype/
-                    cp $MonoLisa-Thin $out/share/fonts/truetype/
-                    cp $MonoLisa-ThinItalic $out/share/fonts/truetype/
+                    cp $MonoLisaBlack $out/share/fonts/truetype/
+                    cp $MonoLisaBlackItalic $out/share/fonts/truetype/
+                    cp $MonoLisaBold $out/share/fonts/truetype/
+                    cp $MonoLisaBoldItalic $out/share/fonts/truetype/
+                    cp $MonoLisaExtraLight $out/share/fonts/truetype/
+                    cp $MonoLisaExtraLightItalic $out/share/fonts/truetype/
+                    cp $MonoLisaLight $out/share/fonts/truetype/
+                    cp $MonoLisaLightItalic $out/share/fonts/truetype/
+                    cp $MonoLisaMedium $out/share/fonts/truetype/
+                    cp $MonoLisaMediumItalic $out/share/fonts/truetype/
+                    cp $MonoLisaRegular $out/share/fonts/truetype/
+                    cp $MonoLisaRegularItalic $out/share/fonts/truetype/
+                    cp $MonoLisaThin $out/share/fonts/truetype/
+                    cp $MonoLisaThinItalic $out/share/fonts/truetype/
                 '';
             };
         };
