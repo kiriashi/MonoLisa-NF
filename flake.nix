0fbcd68db49614d072e8baa4f83257be59201db6
Entity 1011 <goffingslander@gmail.com>
2025-03-27 10:05:03 +0100

    Initial Flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+    inputs =
+    {
+        nixpkgs =
+        {
+            url = "github:NixOS/nixpkgs/nixos-24.11";
+        };
+        MonoLisa =
+        {
+            url = "https://github.com/matomo-org/travis-scripts/raw/refs/heads/master/fonts/Arial.ttf";
+            flake = false;
+        };
+    };
+
+    outputs = inputs:
+    {
+        packages = 
+        {
+            test = inputs.nixpkgs.legacyPackages.x86_64-linux.stdenv.mkDerivation
+            {
+                pname = "MonoLisa Font";
+                version = "0.0.0";
+                src = fetchUrl
+                {
+                    url ="https://github.com/matomo-org/travis-scripts/raw/refs/heads/master/fonts/Arial.ttf";
+                    sha256 = "sha256-NcDzVZ2NtWnjbDEJW4pg1EFkPZX1kTneQOI/ragZuDM=";
+                };
+                installPhase = ''
+                    mkdir -p $out/share/fonts/truetype
+                    mv $src/*.ttf $out/share/fonts/truetype/
+                '';
+            };
+        };
+    };
+}
