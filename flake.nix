32836d0171bfdf1e484bccd1d008f4be4bd4c346
kiriashi <78373893+kiriashi@users.noreply.github.com>
2025-09-09 20:23:35 +0800

    更新 flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "MonoLisa font packaging via flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
